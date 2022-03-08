705339089569e3a5ddda5abde8425ea83561305a
kavishka199808 <it20301590@my.sliit.lk>
2022-03-08 11:34:07 -0800

    error solve

--- app/src/main/java/com/example/tute_1/Display_message.java
+++ app/src/main/java/com/example/tute_1/Display_message.java
@@ -2,7 +2,9 @@ package com.example.tute_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Display_message extends AppCompatActivity {
 
@@ -12,14 +14,16 @@ public class Display_message extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
 
-      // Get the Intent that started this activity and extract the
+        // Get the Intent that started this activity and extract the
+
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
 
-     // Capture the layout's TextView and set the string as its text
+        // Capture the layout's TextView and set the string as its text
 
         TextView textView = findViewById(R.id.tv_display);
         textView.setText(message);
 
     }
+}
